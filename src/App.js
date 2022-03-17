90db2ce28a74eae5c08df29f4b26511f717f41a3
Shreyas <shreyanshrocknroll@gmail.com>
2022-03-17 10:34:57 -0700

    Created Booking Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BookingRoutes from './routes/booking-routes.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <BookingRoutes path="/" exact component={BookingSteps} />
+      </Switch>
+    </Router>
   );
 }
 
