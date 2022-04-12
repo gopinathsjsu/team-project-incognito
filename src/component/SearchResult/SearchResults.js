c88990907fc02a26dd41e9fff2da87907fc60aeb
Mounica Reddy Kandi <99624135+MounicaKandi@users.noreply.github.com>
2022-04-11 21:01:10 -0700

    Updated Overview Page

--- src/component/SearchResult/SearchResults.js
+++ src/component/SearchResult/SearchResults.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import './SearchResult.css';
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import StarIcon from "@material-ui/icons/Star";
-import { useNavigate } from 'react-router';
+import HotelRoomOverview from '../RoomDetails/HotelRoomOverview';
+// import {withRouter} from 'react-router';
+ 
+// import { useNavigate } from 'react-router';
+// import { Navigate } from 'react-router';
 
 function SearchResult({
     img,
@@ -13,18 +18,31 @@ function SearchResult({
     price,
     total,
 }) 
-
-
 {
-    const navigate = useNavigate();
-    const handleOpenImage = () => {
-        console.log("Hello, iM OPENING Hotel overview page");
-        navigate("/hotelRoomOverview");
+    const stateData = {
+        img:img,
+        location:location,
+        title:title,
+        description:description,
+        star:star,
+        price:price,
+        total:total
     }
+    
+// {   const [openHotelOverviewPage, SetOpenHotelOverviewPage] = useState(false);
+    // const navigate = useNavigate();
+    const handleOpenImage = () => {
+        // SetOpenHotelOverviewPage(true);
+        console.log("Hello, iM OPENING Hotel overview page");
+       window.location.pathname ="/roomdetails"
+    };
 
     return (
         <div className='searchResult'>
-            <img onClick={handleOpenImage} src={img} alt="" />
+            <Link to={{pathname: '/roomdetails/', state:{data:stateData}}}>
+                <img style={{width:"550px", height:"300px"}}src ={img}/>
+            </Link>
+            {/* <img onClick={handleOpenImage} src={img} alt="" /> */}
             <FavoriteBorderIcon className="searchResult__heart" />
 
             <div className='searchResult__info'>
@@ -46,6 +64,11 @@ function SearchResult({
                         <h2>{price}</h2>
                         <p>{total}</p>
                     </div>
+
+                    {/* <Link to={{pathname: '/roomdetails/'}}
+                    state={stateName:'mounica'}> go to the hotel </Link> */}
+                    {/* <Link to={{pathname: '/roomdetails/', state:{data:stateData}}}>Go back to Hotel</Link> */}
+
                 </div>
             </div>
         </div>
